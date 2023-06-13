1688646f42b0c0fb1dc0d3f6d52954de32f62d03
Jacob Graham <jakegraham54@gmail.com>
2023-06-12 20:03:11 -0400

    Removed robotjs dependency to instead rely on native Windows .NET framework and powershell to simulate key presses; also dramatically reduced message typing time from 7 seconds to less than 1 second. Refactored code to be more modular and use various small functions.

--- SCUMChatMonitor/app.js
+++ SCUMChatMonitor/app.js
@@ -2,7 +2,6 @@
 
 const { exec } = require('child_process');
 const { discord_bot_token, discord_public_key, discord_secret_key, discord_client_id, discord_guild_id, discord_channel_id } = require('./config.json');
-const robot = require('robotjs');
 const { Client, GatewayIntentBits } = require('discord.js');
 const client_instance = new Client({
     intents: [GatewayIntentBits.Guilds,
@@ -32,31 +31,51 @@ client_instance.on('messageCreate', async (message) => {
 
 client_instance.login(discord_bot_token);
 
+
+function copyToClipboard(text) {
+    const command = `powershell.exe -command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.Clipboard]::SetText('${text.replace(/'/g, "''")}')"`
+    exec(command, (error) => {
+        if (error) {
+            console.error('Error copying to clipboard:', error);
+        } else {
+            console.log('Text copied to clipboard.');
+        }
+    });
+}
+
+function pasteFromClipboard() {
+    const command = `powershell.exe -command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.SendKeys]::SendWait('^v')"`
+    exec(command, (error) => {
+        if (error) {
+            console.error('Error pasting from clipboard:', error);
+        } else {
+            console.log('Text pasted from clipboard.');
+        }
+    });
+}
+
+function pressEnterKey() {
+    const command = `powershell.exe -command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.SendKeys]::SendWait('{Enter}')`;
+    exec(command, (error) => {
+        if (error) {
+            console.error('Error simulating enter key press:', error);
+        } else {
+            console.log('Enter key press simulated');
+        }
+    });
+}
+
 async function runCommand(command) {
+    copyToClipboard(command);
     const scumProcess = exec('powershell.exe -c "Add-Type -TypeDefinition \'using System; using System.Runtime.InteropServices; public class User32 { [DllImport(\"user32.dll\")] public static extern bool SetForegroundWindow(IntPtr hWnd); }\'"');
     if (!scumProcess) {
         return;
     }
 
+    await sleep(500);
+    pasteFromClipboard();
     await sleep(250);
-    robot.typeString(command);
-    robot.keyTap('enter');  
-        /*const powershell = spawn('powershell.exe', ['-NoExit']);
-
-        powershell.stdout.on('data', (data) => {
-            console.log(data.toString());
-        });
-
-        powershell.stderr.on('data', (data) => {
-            console.error(data.toString());
-        });
-        powershell.stdin.write(`Add-Type -TypeDefinition 'using System; using System.Runtime.InteropServices; public class User32 { [DllImport("user32.dll")] public static extern bool SetForegroundWindow(IntPtr hWnd); }'\n`);
-        powershell.stdin.write(`[System.Windows.Forms.SendKeys]::SendWait('t')\n`);
-        powershell.stdin.write(`Start-Sleep -Milliseconds 300\n`);
-        powershell.stdin.write(`[System.Windows.Forms.Clipboard]::SetText('${command}')\n`);
-        powershell.stdin.write(`[System.Windows.Forms.SendKeys]::SendWait('^v')\n`);
-        powershell.stdin.write(`[System.Windows.Forms.SendKeys]::SendWait('{Enter}{Escape}')\n`);
-        powershell.stdin.end();*/
+    pressEnterKey();
 }
 function sleep(milliseconds) {
     return new Promise((resolve) => setTimeout(resolve, milliseconds));
