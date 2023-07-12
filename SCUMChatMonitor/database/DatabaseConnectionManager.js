46c7d2308cda14a20a3757004b930903eb2d0eac
Jacob Graham <jakegraham54@gmail.com>
2023-07-11 21:50:19 -0400

    Added a function in DatabaseConnectionManager which acts as a database trigger, increasing the value in the field 'user_welcome_pack_cost' by 5000 each time the 'user_welcome_pack_uses' increases by 1

--- SCUMChatMonitor/database/DatabaseConnectionManager.js
+++ SCUMChatMonitor/database/DatabaseConnectionManager.js
@@ -6,10 +6,37 @@ module.exports = class DatabaseConnectionManager {
     database_url = process.env.mongodb_connection_string;
     database_connection_pool_size = 10;
     database_name = process.env.mongodb_database_name;
+    database_collection = 'Users';
     pool = null;
+    change_stream_for_welcome_pack_cost = null;
 
     constructor() {
         this.initializeDatabaseConnectionPool();
+        this.initializeChangeStreamForWelcomePackCost();
+    }
+
+    async initializeChangeStreamForWelcomePackCost() {
+        this.change_stream_for_welcome_pack_cost = new MongoClient(this.database_url, {
+            serverApi: {
+                version: ServerApiVersion.v1,
+                strict: true,
+                deprecationErrors: true,
+            }
+        });
+        await this.change_stream_for_welcome_pack_cost.connect();
+        const database = this.change_stream_for_welcome_pack_cost.db(this.database_name);
+        const collection = database.collection(this.database_collection);
+        const change_stream_for_users_collection = collection.watch();
+
+        change_stream_for_users_collection.on("change", async (change) => {
+            if (change.operationType === 'update' && 'user_welcome_pack_uses' in change.updateDescription.updatedFields) {
+                const connection = await this.getConnection();
+                const filter = { _id: change.documentKey._id };
+                const updated_welcome_pack_cost_document = { $inc: { user_welcome_pack_cost: 5000 } };
+                await connection.collection(this.database_collection).updateOne(filter, updated_welcome_pack_cost_document);
+                this.releaseConnection(connection);
+            }
+        });
     }
 
     async initializeDatabaseConnectionPool() {
