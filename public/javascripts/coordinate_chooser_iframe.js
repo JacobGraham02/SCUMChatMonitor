c247f7b136ed65e05c01617008f05a80e7593e9d
Jacob Graham <jakegraham54@gmail.com>
2024-04-07 15:56:33 -0400

    Added functionality to quickly copy your a teleport XYZ coordinate set from the SCUM online interactive map

--- public/javascripts/coordinate_chooser_iframe.js
+++ public/javascripts/coordinate_chooser_iframe.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const toggle_map_view_button = document.querySelector("#toggle_map_view_button"); 
     const toggle_form_view_button = document.querySelector("#toggle_form_view_button");
     const map_selection_tool = document.querySelector("#map_selection_tool");
+    const iframe_instructions_section = document.querySelector("#iframe_instructions_section");
 
     // Initially hide the form
     coordinate_data_form.style.display = "none";
@@ -11,6 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
     toggle_map_view_button.addEventListener("click", function() {
         map_selection_tool.style.display = "block";
         coordinate_data_form.style.display = "none";
+        iframe_instructions_section.style.display = "block";
         this.style.display = "none"; // Hide "Show Map" button
         toggle_form_view_button.style.display = "inline-block"; // Show "Show Form" button
     });
@@ -19,7 +21,28 @@ document.addEventListener('DOMContentLoaded', function() {
     toggle_form_view_button.addEventListener("click", function() {
         coordinate_data_form.style.display = "block";
         map_selection_tool.style.display = "none";
+        iframe_instructions_section.style.display = "none";
         this.style.display = "none"; // Hide "Show Form" button
         toggle_map_view_button.style.display = "inline-block"; // Show "Show Map" button
+    });
+
+    /*
+    The current clipboard contains text in the following format: "#Teleport xx.xx yy.yy zz.zz"
+    Therefore, to get individual components of the string, we must split the string on spaces, and store
+    the individual parts in an array. Then, we can supply the strings located at the array element locations.
+    The teleport command format will always be the same, so it is safe to access array indices directly
+    without worrying about the wrong data. 
+    */  
+    coordinate_data_form.addEventListener("paste", function(event) {
+        const paste_text = (event.clipboardData).getData('text');
+        if (paste_text.startsWith("#Teleport")) {
+            const coordinate_text = paste_text.split(" ")
+            const x_coordinate_input = document.querySelector("#x_coordinate_input");
+            const y_coordinate_input = document.querySelector("#y_coordinate_input");
+            const z_coordinate_input = document.querySelector("#z_coordinate_input");
+            x_coordinate_input.value = coordinate_text[1].trim();
+            y_coordinate_input.value = coordinate_text[2].trim();
+            z_coordinate_input.value = coordinate_text[3].trim();
+        }
     });
 });
