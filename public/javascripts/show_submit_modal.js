0c27a641cd7b617ee26bbbe6eee497097f0bf262
Jacob Graham <jakegraham54@gmail.com>
2024-06-15 22:28:46 -0400

    Added a cancel confirmation dialog to each page that allows a user to cancel their action

--- public/javascripts/show_submit_modal.js
+++ public/javascripts/show_submit_modal.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     const form = document.querySelector(".bot_data_form");
     const submitButton = document.getElementById('save_changes_button');
+    const cancelButton = document.getElementById('cancel_changes_button');
     const modal = $('#submitFormModal'); // Using jQuery for Bootstrap modal
     const modalSubmitButton = document.getElementById('submitButton');
+    const cancelModal = $('#cancelModal'); // Using jQuery for Bootstrap modal
+    const cancelModalConfirmButton = document.getElementById("confirmCancelButton");
 
     submitButton.addEventListener('click', function(event) {
         event.preventDefault(); 
@@ -11,6 +14,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
     modalSubmitButton.addEventListener('click', function() {
         form.submit(); // Submit the form
+    });
+
+    cancelButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        cancelModal.modal('show');
+    });
+
+    cancelModalConfirmButton.addEventListener('click', function() {
+        window.history.back();
     });
 
     // Check for the presence of modals on the page
